7a0401b4c8b2d428ae3d650c09d545a747e50015
Shahriar Mahir <shahriarmahir114@gmail.com>
2026-04-03 19:56:41 +0600

    added black enemy cat

---
+++ scripts/enemy_black_cat.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+
+const SPEED = 150.0
+
+# Start moving Left (-1)
+var direction: int = -1:
+	set(value):
+		direction = value
+		if sprite:
+			# If it was moving right while looking left, we swap this:
+			# This now says: Flip ONLY when moving Left (-1)
+			sprite.flip_h = (direction == -1)
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	# Initial direction
+	direction = -1
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Detect wall and reverse direction
+	if is_on_wall():
+		direction *= -1
+
+	velocity.x = direction * SPEED
+	move_and_slide()
