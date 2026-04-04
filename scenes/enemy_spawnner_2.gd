ff0a885859b332c7c3ecc2c61a6e03db683e3231
Md. Fahim Imam <150895968+Fahim-Codespace@users.noreply.github.com>
2026-04-04 11:43:34 +0600

    EnemyOrangeCat added
    A new enemy..its always the orange catto...

---
+++ scenes/enemy_spawnner_2.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# 1. Drag your enemy .tscn file here from the FileSystem
+@export var enemy_scene: PackedScene = preload("res://scenes/enemy_orange_cat.tscn")
+
+func _on_timer_timeout() -> void:
+	# 2. Create a new copy of the enemy
+	var enemy_instance = enemy_scene.instantiate()
+	
+	# 3. Set the enemy's starting position to the spawner's position
+	enemy_instance.global_position = global_position
+	
+	# 4. Add the enemy to the main game scene
+	get_tree().current_scene.add_child(enemy_instance)
